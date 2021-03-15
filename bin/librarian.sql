06c942d72b54276363c124d03a315cc5546645ae
Suraj Limbalkar <67497720+suraj-limbalkar@users.noreply.github.com>
2021-03-15 20:43:21 +0530

    Add files via upload

---
+++ bin/librarian.sql
@@ -0,0 +1,44 @@
+
+
+SET SQL_MODE="NO_AUTO_VALUE_ON_ZERO";
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8 */;
+
+--
+-- Database: `test`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `librarian`
+--
+
+CREATE TABLE IF NOT EXISTS `librarian` (
+  `id` int(5) NOT NULL AUTO_INCREMENT,
+  `name` varchar(100) NOT NULL,
+  `password` varchar(100) NOT NULL,
+  `email` varchar(100) NOT NULL,
+  `address` varchar(200) NOT NULL,
+  `city` varchar(100) NOT NULL,
+  `contact` varchar(20) NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=7 ;
+
+--
+-- Dumping data for table `librarian`
+--
+
+INSERT INTO `librarian` (`id`, `name`, `password`, `email`, `address`, `city`, `contact`) VALUES
+(1, 'Prabhakar', 'ppp', 'prabhakar@gmail.com', '#445 3rd main 5th cross', 'noida', '9998328238'),
+(4, 'sumedh', 'sumesh', 'sumesh@gmail.com', 'Kuch Bhi', 'noida', '93823932823'),
+(6, 'abhi', 'abhi', 'abhi@gmail.com', '#365 6th main road,2nd cross', 'noida', '92393282323');
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
---
+++ src/Library.java
@@ -0,0 +1,100 @@
+
+import java.awt.EventQueue;
+
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+import javax.swing.border.EmptyBorder;
+
+import javax.swing.ImageIcon;
+import javax.swing.JLabel;
+import java.awt.Color;
+import java.awt.Font;
+import javax.swing.JButton;
+
+import java.awt.event.ActionListener;
+import java.awt.event.ActionEvent;
+
+import javax.swing.SwingConstants;
+import javax.swing.UIManager;
+
+public class Library extends JFrame {
+	static Library frame;
+	private JPanel contentPane;
+
+	/**
+	 * Launch the application.
+	 */
+	public static void main(String[] args) {
+		EventQueue.invokeLater(new Runnable() {
+			public void run() {
+				try {
+					frame= new Library();
+					frame.setVisible(true);
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+			}
+		});
+	}
+
+	/**
+	 * Create the frame.
+	 */
+	public Library() {
+		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		setBounds(100, 100, 1247, 699);
+		contentPane = new JPanel();
+		contentPane.setBackground(new Color(255, 255, 255));
+		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
+		setContentPane(contentPane);
+		
+		JButton btnAdminLogin = new JButton("Admin Login");
+		btnAdminLogin.setBackground(new Color(0, 0, 0,80));
+		btnAdminLogin.setForeground(new Color(255, 255, 204));
+		btnAdminLogin.setBounds(477, 362, 337, 52);
+		btnAdminLogin.setBorderPainted(false);
+		btnAdminLogin.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+			AdminLogin.main(new String[]{});
+			frame.dispose();
+			}
+		});
+		btnAdminLogin.setFont(new Font("Century Schoolbook L", Font.BOLD, 24));
+		
+		JButton btnLibrarianLogin = new JButton("Librarian Login");
+		btnLibrarianLogin.setForeground(new Color(255, 255, 204));
+		btnLibrarianLogin.setBackground(new Color(0, 0,0,80));
+		btnLibrarianLogin.setBounds(477, 466, 337, 53);
+		btnLibrarianLogin.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent arg0) {
+				LibrarianLogin.main(new String[]{});
+			}
+		});
+		btnLibrarianLogin.setFont(new Font("Century Schoolbook L", Font.BOLD, 23));
+		contentPane.setLayout(null);
+		contentPane.add(btnLibrarianLogin);
+		contentPane.add(btnAdminLogin);
+		
+		JPanel panel_1 = new JPanel();
+		panel_1.setBorder(UIManager.getBorder("Menu.border"));
+		panel_1.setBackground(new Color(0, 0,0,80));
+		panel_1.setBounds(229, 93, 794, 139);
+		contentPane.add(panel_1);
+		panel_1.setLayout(null);
+		
+		JLabel lblLibraryManagement = new JLabel("E-Library Management System ");
+		lblLibraryManagement.setBounds(99, 43, 566, 63);
+		panel_1.add(lblLibraryManagement);
+		lblLibraryManagement.setForeground(new Color(255, 255, 255));
+		lblLibraryManagement.setFont(new Font("Chilanka", Font.BOLD, 37));
+		
+
+		
+		JLabel lblNewLabel = new JLabel("",new ImageIcon("image/img1.jpg"),SwingConstants.CENTER);
+		lblNewLabel.setForeground(new Color(0, 0, 0));
+		lblNewLabel.setBounds(-18,-80,1343,749);
+		contentPane.add(lblNewLabel);
+	}
+}
+
+
